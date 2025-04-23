2235769491f5698d761ddc03eaea654229eddbf7
Jensen Bean <jensen.bean@gmail.com>
2025-04-22 23:40:00 -0600

    refactor

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const mainRouter = require("./routes/index");
-const { useEffect } = require("react");
 const app = express();
 
 app.use(
